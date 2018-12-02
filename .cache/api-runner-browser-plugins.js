09244a8802a18ae553f88f45a30a3958072de08e
Sushil Tailor <sushil.tailor@gmail.com>
2018-12-01 23:22:47 -0500

    Deleting node modules folder

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('C:/Users/Sushil/jupio-labs/JupioLabs.github.io/node_modules/gatsby-plugin-google-analytics/gatsby-browser'),
-      options: {"plugins":[],"trackingId":"UA-125456641-3","head":false,"anonymize":true,"respectDNT":true},
-    }]
--- .cache/sync-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-
-exports.components = {
-  "component---src-pages-index-tsx": preferDefault(require("C:\\Users\\Sushil\\jupio-labs\\JupioLabs.github.io\\src\\pages\\index.tsx")),
-  "component---src-pages-privacy-policy-tsx": preferDefault(require("C:\\Users\\Sushil\\jupio-labs\\JupioLabs.github.io\\src\\pages\\privacy-policy.tsx"))
-}
-
