a295472b27c8302bc9e4a3741302fa298b8daa6a
Sushil Tailor <sushil.tailor@gmail.com>
2018-12-01 23:14:35 -0500

    Clearing Master

---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-pages-index-tsx": () => import("C:\\Users\\Sushil\\jupio-labs\\JupioLabs.github.io\\src\\pages\\index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */),
+  "component---src-pages-privacy-policy-tsx": () => import("C:\\Users\\Sushil\\jupio-labs\\JupioLabs.github.io\\src\\pages\\privacy-policy.tsx" /* webpackChunkName: "component---src-pages-privacy-policy-tsx" */)
+}
+
+exports.data = () => import("C:\\Users\\Sushil\\jupio-labs\\JupioLabs.github.io\\.cache\\data.json")
+
