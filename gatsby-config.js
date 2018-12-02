49c552084b166843492ef66bb9fb725116f9cdaa
Diogo Pinto <h.diogopinto@live.com>
2018-12-02 00:24:30 -0500

    added favicon and favicon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,27 @@
 module.exports = {
-
-
   siteMetadata: {
-    // Home Page 
+    // Home Page
 
     index: {
       name: `Jupio Labs`,
       tagline: `Your Innovation Partner`,
       description: `At Jupio Labs we bring our clients to the modern web, using agile & lean approaches to development and design.`,
       keywords: [
-        'HTML',
-        'CSS',
-        'ReactJS',
-        'NodeJS',
-        'Node',
-        'React',
-        'Agile',
-        'Lean',
-        'UX',
-        'UI',
-        'ES6',
-        'JavaScript',
+        "HTML",
+        "CSS",
+        "ReactJS",
+        "NodeJS",
+        "Node",
+        "React",
+        "Agile",
+        "Lean",
+        "UX",
+        "UI",
+        "ES6",
+        "JavaScript"
       ],
-      author: 'Jupio Media Inc.',
-      canonicalLink: `https://www.jupiolabs.com/`,
+      author: "Jupio Media Inc.",
+      canonicalLink: `https://www.jupiolabs.com/`
     },
 
     // Other Pages
@@ -33,27 +31,23 @@ module.exports = {
       tagline: `Privacy Policy`,
       description: `At Jupio Labs we bring our clients to the modern web, using agile & lean approaches to development and design.`,
       keywords: [
-        'HTML',
-        'CSS',
-        'ReactJS',
-        'NodeJS',
-        'Node',
-        'React',
-        'Agile',
-        'Lean',
-        'UX',
-        'UI',
-        'ES6',
-        'JavaScript',
+        "HTML",
+        "CSS",
+        "ReactJS",
+        "NodeJS",
+        "Node",
+        "React",
+        "Agile",
+        "Lean",
+        "UX",
+        "UI",
+        "ES6",
+        "JavaScript"
       ],
-      author: 'Jupio Media Inc.',
+      author: "Jupio Media Inc.",
       canonicalLink: `https://www.jupiolabs.com/privacy-policy`
-    },
-
-
+    }
   },
-
-  
 
   plugins: [
     `gatsby-plugin-sass`,
@@ -67,7 +61,7 @@ module.exports = {
           `Roboto\:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i` // you can also specify font weights and styles
         ]
       }
-    }, 
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -77,14 +71,20 @@ module.exports = {
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        respectDNT: true
         // Enables Google Optimize using your container Id
         // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // // Any additional create only fields (optional)
         // sampleRate: 5,
         // siteSpeedSampleRate: 10,
         // cookieDomain: "jupiolabs.com",
-      },
+      }
     },
-  ],
-}
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/static/favicon.png"
+      }
+    }
+  ]
+};
